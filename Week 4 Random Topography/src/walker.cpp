069cdd3b2dccd640c9d731222784f091bbdcb0c4
JordiYoung <younj768@newschool.edu>
2017-05-23 11:21:40 -0400

    j

---
+++ Week 4 Random Topography/src/walker.cpp
@@ -0,0 +1,41 @@
+
+#include "walker.h"
+
+walker::walker() {
+	x = 0;
+	y = 0;
+	scale = 5;
+}
+
+
+void walker::update() {
+	float choice = ofRandom(1);
+	cout << choice << endl;
+
+	if (choice < 0.4) x++;
+	else if (choice < 0.5) x--;
+	else if (choice < 0.9) y++;
+	else y--;
+
+	int rangeWidth = (ofGetWidth() / 2) / scale;
+	int rangeHeight = (ofGetHeight() / 2) / scale;
+
+	x = overflow(x, -rangeWidth, rangeWidth);
+	y = overflow(y, -rangeHeight, rangeHeight);
+}
+
+
+void walker::draw() {
+	ofSetColor(0, 20);
+	ofDrawRectangle(x, y, 1, 1);
+}
+
+
+int walker::constrain(int input, int min, int max) {
+	return (input < min) ? min : ((input > max) ? max : input);
+}
+
+
+int walker::overflow(int input, int min, int max) {
+	return (input < min) ? max : ((input > max) ? min : input);
+}
---
+++ Week 4 Random Topography/src/walker.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "ofMain.h"
+
+class walker {
+public:
+	walker();
+
+	void update();
+	void draw();
+
+	int constrain(int, int, int);
+	int overflow(int, int, int);
+
+	int x;
+	int y;
+	int scale;
+};
+
